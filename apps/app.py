ee206d17b46942b5f4f5bc2f22d02ef2cb7ccaba
Rindra Ny Aiko Randriamihamina <105801284+NyAiko@users.noreply.github.com>
2023-11-25 00:18:08 +0300

    New app.py with Streamlit

--- apps/app.py
+++ apps/app.py
@@ -1,37 +1,52 @@
-# import pandas as pd
-import numpy as np
-from flask import Flask,render_template,request
-import pickle
-app = Flask(__name__)
-@app.route('/')
+import streamlit as st
+import joblib
+import pandas as pd
+from log import log_func
+# Load the pre-trained model
+model = joblib.load("gbr.p")
 
-def home():
-    return render_template('home.html')
-@app.route('/',methods=['GET','POST'])
-def predict():
-    if request.method =="POST":
-        file_name = 'gbr_beton.p'
-        with open(file_name,'rb') as pickled:
-             data = pickle.load(pickled)
-             model= data['model']
-             print('model loaded')
-        features = [float(x) for x in request.form.values()]
-        #features = np.array(features)
-        #features = features.reshape(1,-1)
-        #print(features)
-        WC =features[1]/features[0]
-        X = []
-        X.append(WC)
-        i = 2
-        while i<=len(features)-1:
-            X.append(features[i])
-            i = i+1
-        X = np.array(X)
-        X = X.reshape(1,-1)   
-        Y = model.predict(X)
-        Y = round(float(Y))
-        pred = str(Y)+' MPa'
-        print(model.predict(X))
-    return render_template('home.html',prediction=pred)
-if __name__=='__main__':
-    app.run()
+
+def predict(input_data):
+    # Make predictions
+    prediction = model.predict(input_data)
+    
+    return prediction
+
+# Streamlit app
+def main():
+    st.title("Concrete Strength Prediction App")
+
+    # User input for Cement
+    cement = st.slider("Cement (kg/m^3)", min_value=100, max_value=540, value=320)
+
+    # User input for BlastFurenace
+    bf = st.slider("BlastFurenace", min_value=0, max_value=359, value=180)
+
+    # Additional input variables
+    fa = st.slider("FlyAsh", min_value=0, max_value=200, value=100)
+    w = st.slider("Water", min_value=115, max_value=250, value=182)
+    sp = st.slider("Super Plasticier", min_value=0, max_value=32, value=16)
+    ca = st.slider("Coase Aggregate", min_value=800, max_value=1145, value=972)
+    fagg = st.slider("Fine Aggregate", min_value=590, max_value=1000, value=795)
+    age = st.slider("Age", min_value=1, max_value=365, value=28)
+
+    # Create a DataFrame with the user input
+    input_data = pd.DataFrame({
+        'Cement': [cement],
+        'BF': [bf],
+        'FA': [fa],
+        'W': [w],
+        'SP': [sp],
+        'CA': [ca],
+        'FAgg': [fagg],
+        'Age': [age]
+    })
+    result = predict(input_data)
+    st.success(f"Predicted Concrete Strength: {result[0]} MPa")
+    # Button to make predictions
+    #if st.button("Predict"):
+    #    result = predict(input_data)
+    #    st.success(f"Predicted Concrete Strength: {result[0]} MPa")
+
+if __name__ == "__main__":
+    main()
